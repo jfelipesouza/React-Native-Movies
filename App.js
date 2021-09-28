df98f4a5dd951e09892de3d55d6411880c94bec0
Felipe Souza <j.felipe.dev.18@gmail.com>
2021-09-27 22:23:23 -0300

    Iniciando projeto

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Testando</Text>
+      <Text>HELLO WORD</Text>
       <StatusBar style="auto" />
     </View>
   );
